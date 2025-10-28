b20e6260fb6420ad4869e5db4fcd575a990363e1
kmwalsh211 <kmwalsh211@gmail.com>
2025-10-28 16:56:00 -0400

    Add Plant Catalog Service starter project (Spring Boot + Gradle + H2)

---
+++ plant_catalog_service/src/test/java/com/example/plantcatalog/PlantCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.plantcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
